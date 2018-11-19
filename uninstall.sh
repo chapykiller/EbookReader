23570e01af16d4d189cf710a66613832e65249f0
Chaps <chapeutyler@gmail.com>
2018-11-19 02:18:54 -0200

    Initial version of uninstall shell script

---
+++ uninstall.sh
@@ -0,0 +1,5 @@
+conda remove -yn quote --all
+conda remove -yn deepvoice --all
+rm -f deepvoice3_pytorch/20171222_deepvoice3_vctk108_checkpoint_step000300000.pth
+rm -f RNNQuoteAttribution/glove.6B.100d.word2vec.txt
+rm -rf RNNQuoteAttribution/weights
